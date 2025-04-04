d247749042b2067c4265ac7ef67bd3da931247a5
andrerizzo@hotmail.com <andrerizzo@gmail.com>
2025-04-04 11:48:50 -0300

    Inclusão do modelo ResNet50v2

--- src/build_model.py
+++ src/build_model.py
@@ -8,11 +8,15 @@ Functions:
 
     build_model_vgg16(input_shape, num_classes)
     compile_model_vgg16(model, learning_rate)
+
+    build_model_vgg16_v2(input_shape, num_classes)
+    compile_model_vgg16_v2(model, learning_rate)
     
     build_model_resnet50(input_shape, num_classes)
     compile_model_resnet50(model, learning_rate)
 
-
+     build_model_resnet50_v2(input_shape, num_classes)
+    compile_model_resnet50_v2(model, learning_rate)
 '''
 
 import tensorflow as tf
@@ -150,6 +154,43 @@ def build_model_resnet50(input_shape=(224, 224, 3), num_classes=4):
 
     model_resnet50 = Model(inputs=base_model_resnet50.input, outputs=predictions_resnet50)
     return model_resnet50
+
+
+'''
+    Modelo ResNet50 versão 2
+        - 20 camadas convolucionais descongeladas 
+        - Demais camadas convolucionais utilizando os pesos originais.
+        - Classificador com uma camada densa e 256 neurônios.
+        - Softmax como função de ativação.
+'''
+
+def build_model_resnet50_v2(input_shape=(224, 224, 3), num_classes=4):
+    '''
+    Constrói o modelo CNN com base no ResNet50 pré-treinado (sem as top layers).
+
+    Args:
+        input_shape (tuple): Formato da imagem de entrada.
+        num_classes (int): Número de classes de saída.
+
+    Returns:
+        model_resnet50_v2 (tf.keras.Model): Modelo compilado.
+    '''
+    # Carregar ResNet50 sem as camadas densas (top) e com pesos da ImageNet
+    base_model_resnet50_v2 = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+
+    # Congelar as camadas convolucionais do VGG16
+    for layer in base_model_resnet50_v2.layers[-20:]:
+        layer.trainable = True
+
+    # Adicionar novas camadas densas customizadas
+    x = base_model_resnet50_v2.output
+    x = Flatten()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    predictions_resnet50_v2 = Dense(num_classes, activation='softmax')(x)
+
+    model_resnet50_v2 = Model(inputs=base_model_resnet50_v2.input, outputs=predictions_resnet50_v2)
+    return model_resnet50_v2
 
 
 def compile_model_resnet50(model, learning_rate=0.0001):
